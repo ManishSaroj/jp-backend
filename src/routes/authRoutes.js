e881eaeae575f717baabbd92a50f4c1a70af6c93
ManishSaroj <manishsaroj5259@gmail.com>
2024-07-06 03:38:18 +0530

    .

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middlewares/authMiddleware'); // Import the middleware
-const checkAdmin = require('../middlewares/adminAuthMiddleware');
+const checkAdminAuth = require('../middlewares/adminAuthMiddleware');
 
 router.get('/checkAuth', checkAuth, (req, res) => {
   // This route is protected and requires authentication
@@ -11,7 +11,7 @@ router.get('/checkAuth', checkAuth, (req, res) => {
 });
 
 // Route that requires admin authentication
-router.get('/checkAdminAuth', checkAuth, checkAdminAuth, (req, res) => {
+router.get('/checkAdminAuth', checkAdminAuth, (req, res) => {
   // This route is protected and requires both general authentication and admin authentication
   res.status(200).json({ success: true, message: "Authenticated as admin", admin: req.admin });
 });
